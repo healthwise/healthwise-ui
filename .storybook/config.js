d250a9a09ad5f95dbc962ed0d15aa0796b68e115
Trevor Eyre <teyre@healthwise.org>
2019-01-29 18:24:26 -0700

    Updated storybook config to support CSS modules

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,27 @@
+import React from 'react'
 import { configure, addDecorator } from '@storybook/react'
 import { withInfo } from '@storybook/addon-info'
+import CssBaseline from '../packages/core/CssBaseline'
+import Theme from '../packages/core/Theme'
+
+const theme = Theme()
 
 addDecorator(withInfo)
+addDecorator(story => (
+  <div
+    style={{
+      padding: '16px',
+      minHeight: '100vh',
+      ...Object.entries(theme).reduce((vars, [key, value]) => {
+        vars['--' + key] = value
+        return vars
+      }, {})
+    }}
+  >
+    <CssBaseline />
+    {story()}
+  </div>
+))
 
 // Automatically import all files ending in *.stories.js
 const req = require.context(
--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,4 +1,14 @@
 module.exports = (baseConfig, env, defaultConfig) => {
-  defaultConfig.module.rules[0].exclude = [/[/\\\\]node_modules[/\\\\]/]
+  const { rules } = defaultConfig.module
+  const jsLoader = rules.find(rule => String(rule.test) === String(/\.(mjs|jsx?)$/))
+  const cssLoader = rules.find(rule => String(rule.test) === String(/\.css$/))
+
+  jsLoader.exclude = [/[/\\\\]node_modules[/\\\\]/]
+  cssLoader.use[1].options = {
+    importLoaders: 1,
+    modules: true,
+    localIdentName: '[name]--[local]'
+  }
+
   return defaultConfig
 }
