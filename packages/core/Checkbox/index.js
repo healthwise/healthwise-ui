54e1094f81f1a9020e1308048dd0227e53198e28
Trevor Eyre <teyre@healthwise.org>
2019-02-27 12:11:14 -0700

    Refactored Checkbox to use styled-components

--- packages/core/Checkbox/index.js
+++ packages/core/Checkbox/index.js
@@ -1,35 +1,113 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import classNames from 'classnames'
+import styled from 'styled-components'
 
+import { defaultTheme } from '../Theme'
 import { CheckboxCheckedIcon, CheckboxUncheckedIcon } from '../Icon'
-import styles from './Checkbox.css'
+
+const Root = styled.div`
+  position: relative;
+  display: block;
+`
+
+const CheckedIconContainer = styled.div`
+  display: none;
+  width: 1em;
+  height: 1em;
+  svg {
+    fill: ${props => props.theme.colorTextPrimary};
+  }
+`
+
+CheckedIconContainer.defaultProps = {
+  theme: defaultTheme,
+}
+
+const UncheckedIconContainer = styled.div`
+  display: none;
+  width: 1em;
+  height: 1em;
+  svg {
+    fill: ${props => props.theme.colorTextPrimary};
+  }
+`
+
+UncheckedIconContainer.defaultProps = {
+  theme: defaultTheme,
+}
+
+const Input = styled.input`
+  position: absolute;
+  display: block;
+  width: 1em;
+  height: 1em;
+  margin: 0;
+  padding: 0;
+  border: 0;
+  outline: 0;
+  opacity: 0;
+  cursor: pointer;
+  font-size: inherit;
+
+  :focus ~ ${CheckedIconContainer},
+  :focus ~ ${UncheckedIconContainer} {
+    outline: ${props => props.theme.focusIndicator};
+    outline-offset: ${props => props.theme.focusIndicatorOffset};
+  }
+
+  :disabled {
+    cursor: not-allowed;
+  }
+
+  :disabled ~ ${CheckedIconContainer},
+  :disabled ~ ${UncheckedIconContainer} {
+    opacity: 0.35;
+    pointer-events: none;
+  }
+
+  :invalid ~ ${CheckedIconContainer} svg,
+  :invalid ~ ${UncheckedIconContainer} svg {
+    fill: ${props => props.theme.colorError};
+  }
+
+  :checked ~ ${CheckedIconContainer} {
+    display: block;
+  }
+
+  :not(:checked) ~ ${UncheckedIconContainer} {
+    display: block;
+  }
+`
+
+Input.defaultProps = {
+  theme: defaultTheme
+}
 
 class Checkbox extends React.Component {
   render() {
-    const { className, name, value, checked, onClick, required, ...otherProps } = this.props
-
-    const checkboxClassName = classNames('hw-checkbox', styles.checkbox, className)
+    const { className, name, value, checked, onClick, required, theme, ...otherProps } = this.props
 
     return (
-      <div className={`hw-checkbox-wrapper ${styles.checkbox_wrapper}`}>
-        <input
+      <Root className="hw-checkbox-wrapper">
+        <Input
           type="checkbox"
           name={name}
           value={value}
           checked={checked}
           required={required}
-          className={checkboxClassName}
+          className={classNames('hw-checkbox', className)}
           onClick={onClick}
+          theme={theme}
           {...otherProps}
         />
-        <div className={`${styles.icon} ${styles.checked}`}>
+        <CheckedIconContainer>
           <CheckboxCheckedIcon role="presentation" />
-        </div>
-        <div className={`${styles.icon} ${styles.unchecked}`}>
+        </CheckedIconContainer>
+        <UncheckedIconContainer>
           <CheckboxUncheckedIcon role="presentation" />
-        </div>
-      </div>
+        </UncheckedIconContainer>
+      </Root>
     )
   }
 }
@@ -41,6 +119,12 @@ Checkbox.propTypes = {
   checked: PropTypes.bool,
   onClick: PropTypes.func,
   required: PropTypes.bool,
+  theme: PropTypes.shape({
+    colorTextPrimary: PropTypes.string,
+    focusIndicator: PropTypes.string,
+    focusIndicatorOffset: PropTypes.string,
+    colorError: PropTypes.string,
+  })
 }
 
 export default Checkbox
