a83bb7479e912855f09539f7153930dfabc40b6a
Nate Logan <nlogan@healthwise.org>
2019-02-27 11:05:26 -0700

    Textarea update to include `value` and `defaultValue` (now can be used as controlled OR uncontrolled)

--- packages/core/TextArea/TextArea.stories.js
+++ packages/core/TextArea/TextArea.stories.js
@@ -4,13 +4,17 @@ import { storiesOf } from '@storybook/react'
 import TextArea from './index'
 
 storiesOf('Form Controls/Textarea', module)
-  .add('with defaults', () => (
+  .addWithInfo('with defaults', `Demonstrates default rendering of TextArea component`, () => (
+    <TextArea label="TextArea Label" />
+  ))
+  .addWithInfo('with default value', `Demonstrates TextArea with max characters`, () => (
     <TextArea label="TextArea Label" maxCharacters="100" />
-  ), {
-  info: `Demonstrates default rendering of TextArea component`
-})
-  .add('with value', () => (
-    <TextArea label="TextArea Label" maxCharacters="100" value="Initial value" />
-  ), {
-  info: `Demonstrates default rendering of TextArea component`
-})
+  ))
+  .addWithInfo(
+    'with default value',
+    `Demonstrates uncontrolled rendering of TextArea component`,
+    () => <TextArea label="TextArea Label" defaultValue="Initial value (uncontrolled component)" />
+  )
+  .addWithInfo('with value', `Demonstrates controlled TextArea component`, () => (
+    <TextArea label="TextArea Label" value="Controlled value" />
+  ))
