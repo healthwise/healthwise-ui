7d0dcffd6c5c0dcd5fc8023b323f09beaf67b98f
Nate Logan <nlogan@healthwise.org>
2019-02-27 11:50:44 -0700

    Fixed TextArea stories

--- packages/core/TextArea/TextArea.stories.js
+++ packages/core/TextArea/TextArea.stories.js
@@ -1,20 +1,28 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
+import { action } from '@storybook/addon-actions'
 
 import TextArea from './index'
 
 storiesOf('Form Controls/Textarea', module)
-  .addWithInfo('with defaults', `Demonstrates default rendering of TextArea component`, () => (
+  .add('with defaults', () => (
     <TextArea label="TextArea Label" />
-  ))
-  .addWithInfo('with default value', `Demonstrates TextArea with max characters`, () => (
+  ), {
+    info: `Demonstrates default rendering of TextArea component`
+  })
+  .add('with max characters', () => (
     <TextArea label="TextArea Label" maxCharacters="100" />
-  ))
-  .addWithInfo(
-    'with default value',
-    `Demonstrates uncontrolled rendering of TextArea component`,
-    () => <TextArea label="TextArea Label" defaultValue="Initial value (uncontrolled component)" />
-  )
-  .addWithInfo('with value', `Demonstrates controlled TextArea component`, () => (
-    <TextArea label="TextArea Label" value="Controlled value" />
-  ))
+  ), {
+    info: `Demonstrates TextArea with max characters`
+  })
+  .add(
+    'with default value', () => (
+    <TextArea label="TextArea Label" defaultValue="Default initial value" />
+  ), {
+    info: `Demonstrates uncontrolled TextArea component`
+  })
+  .add('with value', () => (
+    <TextArea label="TextArea Label" value="Controlled value" onChange={action('changed')} />
+  ), {
+    info: `Demonstrates controlled TextArea component`
+  })
