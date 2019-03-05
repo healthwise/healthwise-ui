9058c7e29cc91a9415aa9f189e83241a4528251c
Trevor Eyre <teyre@healthwise.org>
2019-03-05 11:14:26 -0700

    Added variant option to getThemeVariable function

--- packages/core/Theme/getThemeVariable.js
+++ packages/core/Theme/getThemeVariable.js
@@ -1,15 +1,25 @@
 import upperFirst from 'lodash/upperFirst'
 
-const getThemeVariable = variable => props => {
+// Returns theme variables based on 'color' prop. This is useful
+// in styled-components when you want to set a theme color based
+// on props. The 'variant' option can be used to get the light
+// or dark version of a color.
+//
+// Example:
+// getThemeVariable('color', { variant: 'light' })({ color: 'primary', theme: { ... } })
+// => theme.colorPrimaryLight
+const getThemeVariable = (variable, { variant = undefined } = {}) => props => {
   let { theme, color } = props
+
+  variant = variant ? upperFirst(variant) : undefined
   color = upperFirst(color)
 
   switch(variable) {
     case 'color': {
-      return theme[`color${color}`]
+      return theme[`color${color}${variant || ''}`]
     }
     case 'colorText': {
-      return theme[`colorTextOn${color}`]
+      return theme[`colorTextOn${color}${variant || ''}`]
     }
   }
 }
