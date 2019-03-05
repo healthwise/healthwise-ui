b6aad83a84cffc845b051ebb38701ae707b2b1c0
Trevor Eyre <teyre@healthwise.org>
2019-03-05 11:15:07 -0700

    Refactored ProgressBar to use styled-components. Removed dependency on Material UI LinearProgress component.

--- packages/core/ProgressBar/ProgressBar.stories.js
+++ packages/core/ProgressBar/ProgressBar.stories.js
@@ -21,72 +21,52 @@ storiesOf('UI Feedback/Progress Bar', module)
       />
     </div>
   ), {
-  info: `Demonstrates default rendering of ProgressBar component`
-})
-  .add(
-    'with defaults and color',
-    () => (
-      <div style={styles.container}>
-        <ProgressBar
-          theme="neutral"
-          showPercentage
-          percentComplete={number('Percentage', 45, { range: true, min: 0, max: 100, step: 1 })}
-        />
-      </div>
-    ),
-    {
-      info: `Demonstrates default rendering of ProgressBar component`
-    }
-  )
-  .add(
-    'without text percentage',
-    () => (
-      <div style={styles.container}>
-        <ProgressBar
-          theme="primary"
-          percentComplete={number('Percentage', 90, { range: true, min: 0, max: 100, step: 1 })}
-        />
-      </div>
-    ),
-    {
-      info: `Demonstrates default rendering of ProgressBar component without percentage`
-    }
-  )
-  .add(
-    'using indeterminate type',
-    () => (
-      <div style={styles.container}>
-        <ProgressBar
-          theme="primary"
-          progressBarType="indeterminate"
-          showPercentage
-          percentComplete={number('Percentage', 45, { range: true, min: 0, max: 100, step: 1 })}
-        />
-      </div>
-    ),
-    {
-      info: `Demonstrates rendering of ProgressBar the indeterminate type`
-    }
-  )
+    info: `Demonstrates default rendering of ProgressBar component`
+  })
+  .add('with defaults and color', () => (
+    <div style={styles.container}>
+      <ProgressBar
+        color="neutral"
+        showPercentage
+        percentComplete={number('Percentage', 45, { range: true, min: 0, max: 100, step: 1 })}
+      />
+    </div>
+  ), {
+    info: `Demonstrates default rendering of ProgressBar component`
+  })
+  .add('without text percentage', () => (
+    <div style={styles.container}>
+      <ProgressBar
+        color="primary"
+        percentComplete={number('Percentage', 90, { range: true, min: 0, max: 100, step: 1 })}
+      />
+    </div>
+  ), {
+    info: `Demonstrates default rendering of ProgressBar component without percentage`
+  })
+  .add('using indeterminate type', () => (
+    <div style={styles.container}>
+      <ProgressBar progressBarType="indeterminate" />
+    </div>
+  ), {
+    info: `Demonstrates rendering of ProgressBar the indeterminate type`
+  })
+ .add('using query type', () => (
+    <div style={styles.container}>
+      <ProgressBar progressBarType="query" />
+    </div>
+  ), {
+    info: `Demonstrates rendering of ProgressBar using the query type`
+  })
   .add('using buffer type', () => (
     <div style={styles.container}>
       <ProgressBar
         progressBarType="buffer"
+        showPercentage
         percentComplete={number('Percentage', 45, { range: true, min: 0, max: 100, step: 1 })}
         buffer={number('Buffer', 60, { range: true, min: 0, max: 100, step: 1 })}
       />
     </div>
   ), {
-  info: `Demonstrates rendering of ProgressBar the buffer type`
-})
-  .add(
-    'using query type',
-    () => (
-      <div style={styles.container}>
-        <ProgressBar theme="primary" progressBarType="query" percentComplete="0" />
-      </div>
-    ),
-    {
-      info: `Demonstrates rendering of ProgressBar using the query type`
-    }
-  )
+    info: `Demonstrates rendering of ProgressBar the buffer type`
+  })
--- packages/core/ProgressBar/index.js
+++ packages/core/ProgressBar/index.js
@@ -1,14 +1,117 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import PropTypes from 'prop-types'
 import classNames from 'classnames'
+import styled, { keyframes } from 'styled-components'
 
-import { withStyles } from '@material-ui/core/styles'
-import LinearProgress from '@material-ui/core/LinearProgress'
+import { defaultTheme, getThemeVariable } from '../Theme'
 
-import { createTheme } from '../Theme'
-import styles from './ProgressBar.css'
+const indeterminateAnimation1 = keyframes`
+  0% {
+    left: -35%;
+    right: 100%;
+  }
+  60% {
+    left: 100%;
+    right: -90%;
+  }
+  100% {
+    left: 100%;
+    right: -90%;
+  }
+`
 
-const themeSettings = createTheme()
+const indeterminateAnimation2 = keyframes`
+  0% {
+    left: -200%;
+    right: 100%;
+  }
+  60% {
+    left: 107%;
+    right: -8%;
+  }
+  100% {
+    left: 107%;
+    right: -8%;
+  }
+`
+
+const bufferAnimation = keyframes`
+  0% {
+    opacity: 1;
+    background-position: 0 -23px;
+  }
+  50% {
+    opacity: 0.1;
+    background-position: -100px -23px;
+  }
+  100% {
+    opacity: 1;
+    background-position: -200px -23px;
+  }
+`
+
+const Root = styled.div`
+  position: relative;
+  padding-top: ${props => props.showPercentage ? '1.25em' : '0'};
+`
+
+const Value = styled.div`
+  position: absolute;
+  top: 0;
+  font-size: 1em;
+  line-height: 1;
+  color: ${props => props.theme.colorTextPrimary};
+`
+
+const BarContainer = styled.div`
+  position: relative;
+  width: 100%;
+  height: 4px;
+  overflow: hidden;
+  transform: ${props => props.progressBarType === 'query' ? 'rotate(180deg)' : 'none'};
+`
+
+const DefaultBar = styled.div`
+  height: 100%;
+  position: absolute;
+  left: 0;
+  z-index: 2;
+  background: ${getThemeVariable('color')};
+`
+
+const IndeterminateBar1 = styled(DefaultBar)`
+  animation: ${indeterminateAnimation1} 2.1s cubic-bezier(0.65, 0.815, 0.735, 0.395) infinite;
+`
+
+const IndeterminateBar2 = styled(DefaultBar)`
+  animation: ${indeterminateAnimation2} 2.1s cubic-bezier(0.165, 0.84, 0.44, 1) infinite;
+  animation-delay: 1.15s;
+`
+
+const Background = styled.div`
+  height: 100%;
+  position: absolute;
+  ${props => props.progressBarType === 'buffer'
+    ? 'left: 0;'
+    : 'right: 0;'
+  }
+  z-index: 1;
+  background: ${getThemeVariable('color', { variant: 'light' })};
+`
+
+const Buffer = styled.div`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  background-image: radial-gradient(${
+    getThemeVariable('color', { variant: 'light' })
+  } 0%, ${
+    getThemeVariable('color', { variant: 'light'})
+  } 16%, transparent 42%);
+  background-size: 10px 10px;
+  background-position: 0px -23px;
+  animation: ${bufferAnimation} 3s infinite linear;
+`
 
 class ProgressBar extends Component {
   _normalizePercentage(percent) {
@@ -22,60 +125,89 @@ class ProgressBar extends Component {
   }
 
   render() {
-    let { className, showPercentage, percentComplete, progressBarType, buffer, theme } = this.props
-    let completed = this._normalizePercentage(percentComplete)
-    percentComplete = completed + '%'
-    let percentageNumber = showPercentage ? (
-      <div
-        className={`hw-progress-bar-number ${styles.progress_bar_number}`}
-        style={{ left: percentComplete }}
-      >
-        {percentComplete}
-      </div>
-    ) : null
+    const {
+      className,
+      showPercentage,
+      percentComplete,
+      progressBarType,
+      buffer,
+      color,
+      theme,
+      ...otherProps
+    } = this.props
 
-    let colorPrimary = themeSettings[`color-${theme}`]
-    let colorSecondary = themeSettings[`color-${theme}-light`]
-    let colorPrimaryDark = themeSettings[`color-${theme}-dark`]
-    let colorDashedBackground = themeSettings['color-background-light']
-    let dashedGradient = `radial-gradient(${colorSecondary} 0%, ${colorPrimaryDark} 16%, transparent 42%)`
-
-    const progressBarStyles = {
-      root: {
-        flexGrow: 1,
-      },
-      barColorPrimary: { backgroundColor: colorPrimary },
-      colorPrimary: { backgroundColor: colorSecondary },
-      dashedColorPrimary: {
-        'background-size': '10px 10px',
-        'background-image': dashedGradient,
-        backgroundColor: colorDashedBackground,
-      },
-      // bar1Determinate: {
-      //   background: 'linear-gradient(to left, #e21e4a, #a92fff)'
-      // }
-    }
-
-    const StyledLinearProgress = withStyles(progressBarStyles)(LinearProgress)
-
-    const progressClass = classNames('hw-progress-bar-fill', styles.progress_bar_fill, className)
+    const percent = this._normalizePercentage(percentComplete)
+    const bufferPercent = this._normalizePercentage(buffer)
 
     return (
-      <div
-        className={`hw-progress-bar ${styles.progress_bar}`}
+      <Root
+        className={classNames('hw-progress-bar', className)}
+        showPercentage={showPercentage}
         aria-valuemin="0"
         aria-valuemax="100"
+        aria-valuenow={percent}
+        {...otherProps}
       >
-        <div className={`hw-progress-bar-container ${styles.progress_bar_container}`}>
-          {percentageNumber}
-          <StyledLinearProgress
-            className={progressClass}
-            variant={progressBarType}
-            value={completed}
-            valueBuffer={buffer}
+        {showPercentage && (
+          <Value
+            className="hw-progress-bar-number"
+            theme={theme}
+            style={{ left: percentComplete + '%' }}
+          >
+            {percent}%
+          </Value>
+        )}
+        <BarContainer className="hw-progress-bar-container" progressBarType={progressBarType}>
+          {progressBarType === 'determinate' && (
+            <DefaultBar
+              className="hw-progress-bar-fill"
+              color={color}
+              theme={theme}
+              style={{ width: percent + '%' }}
+            />
+          )}
+          {(progressBarType === 'indeterminate' || progressBarType === 'query') && (
+            <Fragment>
+              <IndeterminateBar1
+                className="hw-progress-bar-fill"
+                color={color}
+                theme={theme}
+              />
+              <IndeterminateBar2
+                className="hw-progress-bar-fill"
+                color={color}
+                theme={theme}
+              />
+            </Fragment>
+          )}
+          {progressBarType === 'buffer' && (
+            <Fragment>
+              <DefaultBar
+                className="hw-progress-bar-fill"
+                color={color}
+                theme={theme}
+                style={{ width: percent + '%' }}
+              />
+              <Buffer
+                className="hw-progress-bar-buffer"
+                color={color}
+                theme={theme}
+              />
+            </Fragment>
+          )}
+          <Background
+            className="hw-progress-bar-background"
+            progressBarType={progressBarType}
+            color={color}
+            theme={theme}
+            style={{
+              width: progressBarType === 'buffer'
+                ? bufferPercent + '%'
+                : '100%'
+            }}
           />
-        </div>
-      </div>
+        </BarContainer>
+      </Root>
     )
   }
 }
@@ -85,15 +217,24 @@ ProgressBar.propTypes = {
   progressBarType: PropTypes.oneOf(['determinate', 'indeterminate', 'buffer', 'query']),
   buffer: PropTypes.number,
   showPercentage: PropTypes.bool,
-  percentComplete: PropTypes.number.isRequired,
-  theme: PropTypes.oneOf(['primary', 'neutral']),
+  percentComplete: PropTypes.number,
+  color: PropTypes.oneOf(['primary', 'neutral']),
+  theme: PropTypes.shape({
+    colorPrimaryLight: PropTypes.string,
+    colorPrimary: PropTypes.string,
+    colorNeutralLight: PropTypes.string,
+    colorNeutral: PropTypes.string,
+    colorTextPrimary: PropTypes.string,
+  })
 }
 
 ProgressBar.defaultProps = {
   showPercentage: false,
   progressBarType: 'determinate',
+  percentComplete: 0,
   buffer: 0,
-  theme: 'primary',
+  color: 'primary',
+  theme: defaultTheme,
 }
 
 export default ProgressBar
