8647984e686e3a88068f0a5010921d4508739c05
Trevor Eyre <teyre@healthwise.org>
2019-03-06 11:24:28 -0700

    Refactored Message to use styled-components

--- packages/core/Message/index.js
+++ packages/core/Message/index.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import classNames from 'classnames'
+import styled from 'styled-components'
 
+import { defaultTheme, getThemeVariable } from '../Theme'
 import { InfoIcon, WarningIcon } from '../Icon'
-import styles from './Message.css'
 
 const icons = {
   info: <InfoIcon />,
@@ -11,23 +12,60 @@ const icons = {
   error: <WarningIcon />,
 }
 
+// Map align prop to justify-content value
+const alignMap = {
+  left: 'flex-start',
+  center: 'center',
+  right: 'flex-end',
+}
+
+const Root = styled.span`
+  display: flex;
+  align-items: center;
+  justify-content: ${props => alignMap[props.align]};
+  color: ${getThemeVariable('color')};
+
+  svg {
+    fill: ${getThemeVariable('color')}
+  }
+`
+
+const Icon = styled.div`
+  width: 2em;
+  height: 2em;
+  margin-right: 0.5em;
+  flex: 0 0 auto;
+`
+
+const Container = styled.div`
+  font-size: 1em;
+`
+
 class Message extends Component {
   render() {
-    const { className, children, type, align, showIcon, ...otherProps } = this.props
-
-    const containerClass = classNames(
-      'hw-message-container',
-      styles.container,
-      styles[type],
-      styles[align]
-    )
-    const messageClass = classNames('hw-message', `hw-message-${type}`, styles.message, className)
+    const {
+      className,
+      children,
+      type,
+      align,
+      showIcon,
+      theme,
+      ...otherProps
+    } = this.props
 
     return (
-      <span className={containerClass} {...otherProps}>
-        {showIcon && <div className={styles.icon}>{icons[type]}</div>}
-        <div className={messageClass}>{children}</div>
-      </span>
+      <Root
+        className="hw-message-container"
+        align={align}
+        color={type}
+        theme={theme}
+        {...otherProps}
+      >
+        {showIcon && <Icon>{icons[type]}</Icon>}
+        <Container className={classNames('hw-message', `hw-message-${type}`, className)}>
+          {children}
+        </Container>
+      </Root>
     )
   }
 }
@@ -38,12 +76,18 @@ Message.propTypes = {
   type: PropTypes.oneOf(['info', 'warning', 'error']),
   align: PropTypes.oneOf(['left', 'center', 'right']),
   showIcon: PropTypes.bool,
+  theme: PropTypes.shape({
+    colorInfo: PropTypes.string,
+    colorWarning: PropTypes.string,
+    colorError: PropTypes.string,
+  }),
 }
 
 Message.defaultProps = {
   type: 'info',
   align: 'left',
   showIcon: true,
+  theme: defaultTheme,
 }
 
 export default Message
