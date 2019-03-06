7c9b4d77017607045cc15f1dc7d8011faf263999
Trevor Eyre <teyre@healthwise.org>
2019-03-06 15:28:43 -0700

    Refactored TabGroup to use styled-components

--- packages/core/TabGroup/index.js
+++ packages/core/TabGroup/index.js
@@ -1,9 +1,50 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import classNames from 'classnames'
+import styled from 'styled-components'
 
+import { defaultTheme } from '../Theme'
 import Tab from '../Tab'
-import styles from './TabGroup.css'
+
+const Root = styled.ol`
+  display: flex;
+  flex-wrap: nowrap;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: stretch;
+  overflow-x: auto;
+  margin: 0;
+  padding: 0 ${props => props.theme.spacingL};
+  box-sizing: border-box;
+  list-style: none;
+  width: ${props => props.stretch ? '100%' : 'auto'};
+
+  @media screen and (max-width: 750px) {
+    width: 100%;
+    padding: 0;
+  }
+`
+
+const Container = styled.li`
+  display: block;
+  margin: ${props => `${props.theme.spacingXs} ${props.theme.spacingXs} 0`};
+  text-align: center;
+  box-sizing: border-box;
+  flex: ${props => props.stretch ? '1' : '0 1 auto'};
+
+  :first-child {
+    margin-left: 0;
+  }
+
+  :last-child {
+    margin-right: 0;
+  }
+
+  @media screen and (max-width: 750px) {
+    width: inherit;
+    margin: 5px 2px 0;
+  }
+`
 
 class TabGroup extends Component {
   constructor() {
@@ -32,26 +73,22 @@ class TabGroup extends Component {
   }
 
   render() {
-    const { children, className, stretch, ...otherProps } = this.props
-    const tabGroupClassName = classNames({
-      'hw-tab-group': true,
-      [styles.TabGroup]: true,
-      [className]: className,
-      [styles.stretch]: stretch,
-    })
-    const tabContainerClassName = classNames({
-      [styles.tabContainer]: true,
-      [styles.tabContainerStretch]: stretch,
-    })
+    const { children, className, stretch, theme, ...otherProps } = this.props
 
     return (
-      <ol ref={this.tabGroup} className={tabGroupClassName} {...otherProps}>
+      <Root
+        ref={this.tabGroup}
+        className={classNames('hw-tab-group', className)}
+        stretch={stretch}
+        theme={theme}
+        {...otherProps}
+      >
         {React.Children.map(children, (tab, index) => (
-          <li key={index} className={tabContainerClassName}>
+          <Container key={index} stretch={stretch} theme={theme}>
             {tab}
-          </li>
+          </Container>
         ))}
-      </ol>
+      </Root>
     )
   }
 }
@@ -63,10 +100,15 @@ TabGroup.propTypes = {
   ]),
   className: PropTypes.string,
   stretch: PropTypes.bool,
+  theme: PropTypes.shape({
+    spacingXs: PropTypes.string,
+    spacingL: PropTypes.string,
+  })
 }
 
 TabGroup.defaultProps = {
   stretch: true,
+  theme: defaultTheme,
 }
 
 export default TabGroup
