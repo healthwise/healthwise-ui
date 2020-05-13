fbda9ba9fbd387ecbaec363190d6cd23723062af
Nate Logan <nlogan@healthwise.org>
2020-05-13 17:01:54 -0600

    Setup/config for MDX stories; converted Accordion stories to MDX

--- .storybook/addons.js
+++
@@ -1,6 +0,0 @@
-import '@storybook/addon-actions/register'
-import '@storybook/addon-knobs/register'
-import '@storybook/addon-links/register'
-import '@storybook/addon-viewport/register'
-
-import '@dump247/storybook-state/register'
