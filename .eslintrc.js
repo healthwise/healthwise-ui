079052c0c37286ca97fd759d5fec4a19cd4894a9
Marcus Lyons <mlyons@healthwise.org>
2021-04-22 10:53:56 -0600

    chore(deps): updated eslint config; removed eslint prettier; added prettier config; added npm-run-all;

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,24 @@
 // Same configuration as @healthwise-ui/eslint-plugin package
 module.exports = {
-  extends: ['plugin:react-app/recommended', 'plugin:prettier/recommended'],
-  rules: {
-    'react-app/jsx-a11y/href-no-hash': 'off',
-    'prettier/prettier': [
-      'warn',
-      {
-        printWidth: 100,
-        tabWidth: 2,
-        useTabs: false,
-        semi: false,
-        singleQuote: true,
-        trailingComma: 'es5',
-        bracketSpacing: true,
-        jsxBracketSameLine: false,
-        arrowParens: 'avoid',
-      },
-    ],
+  parserOptions: {
+    ecmaVersion: 2019,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
   },
+  extends: [
+    'react-app',
+    'eslint:recommended',
+    'eslint-config-prettier',
+  ],
+  rules: {
+    strict: ['error', 'never'],
+  },
+  env: {
+    browser: true,
+  },
+  ignorePatterns: ['node_modules'],
   globals: {
     __DEV__: 'readonly',
   },
