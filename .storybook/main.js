5deb2ec015fad0d2963af1d2d79b9de98143854a
zohlinger-hw <zohlinger@healthwise.org>
2021-08-12 12:11:54 -0600

    Missed some references to @storybook/addon-knobs

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,12 +1,15 @@
 module.exports = {
-  stories: ['../src/_docs/intro.stories.mdx', '../src/_docs/*.stories.mdx', '../src/**/*.stories.mdx'],
+  stories: [
+    '../src/_docs/intro.stories.mdx',
+    '../src/_docs/*.stories.mdx',
+    '../src/**/*.stories.mdx',
+  ],
   addons: [
     '@storybook/addon-actions',
     '@storybook/addon-links',
     '@storybook/addon-docs',
-    '@storybook/addon-knobs/register',
+    '@storybook/addon-controls',
     '@storybook/addon-viewport/register',
     '@dump247/storybook-state/register',
   ],
-};
-
+}
