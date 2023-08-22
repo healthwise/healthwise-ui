337fc9938636fd41d8d2382c22afa84b6f5bcd67
zohlinger-hw <zohlinger@healthwise.org>
2023-08-22 09:23:47 -0600

    Cleaning up storybook main, removing unused package

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,15 +1,5 @@
 module.exports = {
-  stories: [
-    //'../src/_docs/intro.stories.(js|mdx)',
-    //'../src/_docs/accessibility.stories.(js|mdx)',
-    //'../src/_docs/*.stories.(js|mdx)',
-    '../src/**/*.stories.(js|mdx)',
-    //'../src/Accordion/*.stories.(js|mdx)',
-    //'../src/AccordionGroup/*.stories.(js|mdx)',
-    //'../src/BlockHeading/*.stories.(js|mdx)',
-    //'../src/Button/*.stories.(js|mdx)',
-    //'../src/ButtonGroup/*.stories.(js|mdx)',
-  ],
+  stories: ['../src/**/*.stories.(js|mdx)'],
 
   addons: [
     '@storybook/addon-actions',
@@ -18,7 +8,6 @@ module.exports = {
     '@storybook/addon-controls',
     '@storybook/addon-viewport/register',
     '@dump247/storybook-state/register',
-    '@storybook/addon-mdx-gfm',
   ],
 
   framework: {
